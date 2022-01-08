6b8aa7fb4e842f3ceaaad968b8d62a05af22ecbb
milad hasibi <miladmxm12@gmail.com>
2022-01-08 13:20:23 +0330

    slider auto

--- src/components/ui/slider/costomSlider.jsx
+++ src/components/ui/slider/costomSlider.jsx
@@ -1,13 +1,35 @@
+import { useEffect, useRef } from "react"
 import "./slider.scss"
 const Slider = () => {
+    const interval = useRef(null)
+    const sliderWraper = useRef(null)
+    useEffect(()=>{
+        const inputs = sliderWraper.current.getElementsByTagName("input")
+        inputs[0].checked = true;
+        let cunt = 1
+        interval.current = setInterval(() => {
+            if(cunt < inputs.length){
+                inputs[cunt-1].checked =false
+                inputs[cunt].checked = true
+                cunt=cunt+1;
+            }else{
+                inputs[inputs.length-1].checked = false
+                cunt = 1;
+                inputs[0].checked = true
+            }
+        }, 3000);
+        return()=>{
+            clearInterval(interval.current)
+        }
+    },[])
     return (
         <>
-            <div className="wraperSlider">
-                <input type="radio" name="slide" id="one" checked/>
-                <input type="radio" name="slide" id="two" checked/>
-                <input type="radio" name="slide" id="three" checked/>
-                <input type="radio" name="slide" id="four" checked/>
-                <input type="radio" name="slide" id="five" checked/>
+            <div ref={sliderWraper} className="wraperSlider">
+                <input type="radio" name="slide" id="one" />
+                <input type="radio" name="slide" id="two" />
+                <input type="radio" name="slide" id="three" />
+                <input type="radio" name="slide" id="four" />
+                <input type="radio" name="slide" id="five" />
                 <div className="img img-1">
                     <img src="img/1.png" />
                 </div>
